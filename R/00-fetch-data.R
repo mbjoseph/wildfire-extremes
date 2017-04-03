137e7249518670c1e676fb6c15a7943685c42ab8
Max Joseph <maxwellbjoseph@gmail.com>
2017-04-03 11:53:06 -0600

    automating data download

---
+++ R/00-fetch-data.R
@@ -0,0 +1,56 @@
+library(assertthat)
+
+# Acquiring raw data ------------------------------------------------------
+# This script will check to see whether all of the raw data files exist locally
+# and download any missing files.
+
+raw_prefix <- file.path("data", "raw")
+
+# Level 3 ecoregion data from EPA -----------------------------------------
+
+ecoregion_prefix <- file.path(raw_prefix, "us_eco_l3")
+ecoregion_shp <- file.path(ecoregion_prefix, "us_eco_l3.shp")
+
+if (!file.exists(ecoregion_shp)) {
+  loc <- "ftp://newftp.epa.gov/EPADataCommons/ORD/Ecoregions/us/us_eco_l3.zip"
+  dest <- paste0(ecoregion_prefix, ".zip")
+  download.file(loc, dest)
+  unzip(dest, exdir = ecoregion_prefix)
+  unlink(dest)
+  assert_that(file.exists(ecoregion_shp))
+}
+
+
+
+# MTBS fire data ----------------------------------------------------------
+
+mtbs_prefix <- file.path(raw_prefix, "mtbs_fod_pts_data")
+mtbs_shp <- file.path(mtbs_prefix, 'mtbs_fod_pts_20160401.shp')
+
+if (!file.exists(mtbs_shp)) {
+  loc <- "http://www.mtbs.gov/MTBS_Uploads/data/composite_data/fod_pt_shapefile/mtbs_fod_pts_data.tar.gz"
+  dest <- paste0(mtbs_prefix, ".tar.gz")
+  download.file(loc, dest)
+  untar(dest, exdir = mtbs_prefix)
+  unlink(dest)
+  assert_that(file.exists(mtbs_shp))
+}
+
+
+# Short fire data ---------------------------------------------------------
+
+short_prefix <- file.path(raw_prefix, "short_fire")
+short_sqlite <- file.path(short_prefix, "Data", "FPA_FOD_20150323.sqlite")
+
+if (!file.exists(short_sqlite)) {
+  loc <- "https://www.fs.usda.gov/rds/archive/products/RDS-2013-0009.3/RDS-2013-0009.3_SQLite.zip"
+  dest <- paste0(short_prefix, ".zip")
+  if (!dir.exists(short_prefix)) {
+    dir.create(short_prefix)
+  }
+  download.file(loc, dest, method = "wget")
+  unzip(dest, exdir = short_prefix)
+  unlink(dest)
+  assert_that(file.exists(short_sqlite))
+}
+
--- R/01-clean_data.R
+++ R/01-clean_data.R
@@ -1,49 +1,84 @@
+library(raster)
 library(tidyverse)
 library(rgdal)
 library(maptools)
 library(ggmap)
 library(foreign)
 library(purrr)
-library(raster)
+library(RSQLite)
 
-# load EPA level 3 ecoregion data
-# accessible from:
-# https://www.epa.gov/eco-research/level-iii-and-iv-ecoregions-continental-united-states
-level <- 3
-ecoregions <- readOGR(paste0("data/raw/us_eco_l", level),
-                      paste0("us_eco_l", level))
+source("R/00-fetch-data.R")
 
-# Simplify the polygons for faster plotting & reproject ----------------------
-wgs84 <- "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
+# Read fire data ----------------------
+mtbs <- readOGR(mtbs_prefix, 'mtbs_fod_pts_20160401')
 
+explore_short <- FALSE
+if (explore_short) {
+  # Exploration to explore whether to merge mtbs & short data ----------
+  mtbs_df <- mtbs %>%
+    as.data.frame() %>%
+    tbl_df
+  names(mtbs_df) <- tolower(names(mtbs_df))
+  mtbs_df <- mtbs_df %>%
+    rename(mtbs_id = fire_id,
+           mtbs_fire_name = firename,
+           latitude = lat,
+           longitude = long,
+           fire_size = r_acres)
+
+  short <- dbConnect(RSQLite::SQLite(), short_sqlite) %>%
+    dbGetQuery('select * from fires') %>%
+    dplyr::select(-shape) %>%
+    tbl_df
+
+  # select relevant columns from each
+  mtbs_df <- mtbs_df %>%
+    select(mtbs_id, mtbs_fire_name, fire_year, fire_size, longitude, latitude) %>%
+    mutate(source = "mtbs")
+
+  short <- short %>%
+    mutate(source = "short") %>%
+    select(objectid, fire_name, mtbs_id, mtbs_fire_name, fire_size, fire_year,
+           source, longitude, latitude)
+
+  full_join(mtbs_df, short) %>%
+    filter(fire_year >= min(short$fire_year),
+           fire_size > 1000,
+           !duplicated(mtbs_id)) %>%
+    ggplot(aes(fire_size, color = source)) +
+    geom_density() +
+    scale_x_log10()
+  # Looks like short hardly adds any large fires - they're mostly on the
+  # smaller side. If we care about extremes and longer term temporal patterns,
+  # it may be worth using MTBS only.
+}
+
+## load EPA level 3 ecoregion data ------------------------------------------
+ecoregions <- readOGR(ecoregion_prefix, "us_eco_l3")
+
+# Simplify the polygons for faster plotting & reproject
 ecoregions <- ecoregions %>%
   rgeos::gSimplify(tol = 1e3, topologyPreserve = TRUE) %>%
-  SpatialPolygonsDataFrame(ecoregions@data) %>%
-  spTransform(CRS(wgs84))
+  SpatialPolygonsDataFrame(ecoregions@data)
 
-# Create a data frame from ecoregion data ---------------------------------
 ecoregions$id <- row.names(ecoregions)
 er_df <- fortify(ecoregions, region = 'id')
 er_df <- left_join(er_df, ecoregions@data, by = 'id') %>%
   as_tibble()
 names(er_df) <- tolower(names(er_df))
 
-# Read short and mtbs fire data & merge with ecoregions ----------------------
-# Data from:
-# http://www.mtbs.gov/nationalregional/pointdata.html
-mtbs <- readOGR('data/raw/mtbs_fod_pts_data/', 'mtbs_fod_pts_20160401') %>%
-  subset(!(STATE %in% c("AK", "PR", "HI"))) %>%
-  spTransform(CRS(wgs84))
-
+# overlay mtbs fire data onto ecoregion shapefile
 d <- mtbs %>%
+  spTransform(CRS(proj4string(ecoregions))) %>%
   over(ecoregions) %>%
   bind_cols(as.data.frame(mtbs)) %>%
   map_if(is.factor, as.character) %>%
   as_tibble() %>%
-  filter(!is.na(US_L3NAME)) %>%
+  filter(!is.na(US_L3NAME),
+         R_ACRES > 1000) %>%
   mutate(fire_size = R_ACRES)
 
 names(d) <- tolower(names(d))
 
-## Fetch pdsi at fire locations
-rm(list = c("level", "wgs84",  "mtbs"))
+# clean up
+rm(list = c("mtbs", "explore_short"))
