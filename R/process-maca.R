1808116245522548d19d85437e06fefbac8100d3
Max Joseph <maxwellbjoseph@gmail.com>
2017-06-07 16:05:21 -0600

    generate annual means and produce a coarse grid for MACA climate data

--- R/process-maca.R
+++ R/process-maca.R
@@ -1,5 +1,6 @@
 library(raster)
 library(rgdal)
+library(tidyverse)
 
 maca_files <- list.files("data/raw/maca", pattern = ".nc", full.names = TRUE)
 maca_types <- c("huss", "pr", "rsds", "tasmax", "tasmin", "was")
@@ -28,3 +29,35 @@ for (i in seq_along(to_exclude)) {
   keep <- grep(pattern = to_exclude[i], x = names(r), invert = TRUE, value = TRUE)
   r <- subset(r, keep)
 }
+
+# generate annual mean values
+r_year <- strsplit(names(r), split = "_") %>%
+  lapply(`[[`, 2) %>%
+  unlist() %>%
+  substr(1, 4) %>%
+  parse_number
+
+variable <- strsplit(names(r), split = "_") %>%
+  lapply(`[[`, 1) %>%
+  unlist()
+
+annual_r <- list()
+k <- 1
+for (i in seq_along(unique(r_year))) {
+  for (j in seq_along(unique(variable))) {
+    which_idx <- r_year == unique(r_year)[i] &
+      variable == unique(variable)[j]
+    annual_r[[k]] <- subset(r, names(r)[which_idx]) %>%
+      mean
+    k <- k + 1
+  }
+}
+
+annual_rstack <- stack(annual_r)
+names(annual_rstack) <- expand.grid(unique(variable), unique(r_year)) %>%
+  as.matrix() %>%
+  apply(1, paste, collapse = "_")
+
+coarse_r <- aggregate(annual_rstack[[1]], fact = 4)
+poly_grid <- rasterToPolygons(coarse_r)
+
