42c2bc7a23e68cb991fb2d790bdba59d636d357a
Max Joseph <maxwellbjoseph@gmail.com>
2017-08-04 17:22:41 +0000

    add bash script to fetch data from s3

---
+++ bash/fetch-gridmet-from-s3.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+aws s3 cp s3://earthlab-gridmet/pet data/processed/ --recursive
+aws s3 cp s3://earthlab-gridmet/pr data/processed/ --recursive
+aws s3 cp s3://earthlab-gridmet/vs data/processed/ --recursive
