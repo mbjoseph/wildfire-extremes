f4ea21762b3bfa1f80dc7c9a665a3e75087aeb5b
Max Joseph <maxwellbjoseph@gmail.com>
2017-08-04 11:31:50 -0600

    fixing shebang and paths

--- bash/fetch-gridmet-from-s3.sh
+++ bash/fetch-gridmet-from-s3.sh
@@ -1,5 +1,5 @@
-#!bin/bash
+#!/bin/sh
 
-aws s3 cp s3://earthlab-gridmet/pet data/processed/ --recursive
-aws s3 cp s3://earthlab-gridmet/pr data/processed/ --recursive
-aws s3 cp s3://earthlab-gridmet/vs data/processed/ --recursive
+aws s3 cp s3://earthlab-gridmet/pet ../data/processed/ --recursive
+aws s3 cp s3://earthlab-gridmet/pr ../data/processed/ --recursive
+aws s3 cp s3://earthlab-gridmet/vs ../data/processed/ --recursive
