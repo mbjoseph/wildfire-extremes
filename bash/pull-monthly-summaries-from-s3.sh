1908798e520a35fefa2307f0cbc246fef20b9fc7
Max Joseph <maxwellbjoseph@gmail.com>
2017-08-18 13:31:52 -0600

    adding script to pull climate data from s3

---
+++ bash/pull-monthly-summaries-from-s3.sh
@@ -0,0 +1,4 @@
+aws s3 cp s3://earthlab-gridmet/pet ../data/processed/pet --recursive
+aws s3 cp s3://earthlab-gridmet/pr ../data/processed/pr --recursive
+aws s3 cp s3://earthlab-gridmet/tmmx ../data/processed/tmmx --recursive
+aws s3 cp s3://earthlab-gridmet/vs ../data/processed/vs --recursive
