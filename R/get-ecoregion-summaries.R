a5ff351ea9ad368354be7d3993d5b93d3f6a5d43
Max Joseph <maxwellbJoseph@gmail.com>
2017-08-19 02:40:04 +0000

    finishing processing

--- R/get-ecoregion-summaries.R
+++ R/get-ecoregion-summaries.R
@@ -16,18 +16,45 @@ ecoregion_shp <- spTransform(ecoregion_shp,
                        CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"))
 
 
-tifs <- list.files(pattern = ".tif")
+tifs <- list.files("data/processed",
+                   pattern = ".tif",
+                   recursive = TRUE,
+                   full.names = TRUE)
 
 extract_one <- function(filename, ecoregion_shp) {
   raster::extract(raster::stack(filename), ecoregion_shp,
                   na.rm = TRUE, fun = mean, df = TRUE)
 }
 
-sfInit(parallel = TRUE, cpus = 4)
+sfInit(parallel = TRUE, cpus = 40)
 sfExport(list = c("ecoregion_shp"))
 
-# try on subset
 extractions <- sfLapply(as.list(tifs),
                         fun = extract_one,
                         ecoregion_shp = ecoregion_shp)
 sfStop()
+
+# convert to a data frame
+library(tidyverse)
+ecoregion_summaries <- extractions %>%
+  bind_cols %>%
+  as_tibble %>%
+  mutate(index = ID) %>%
+  select(-starts_with("ID")) %>%
+  rename(ID = index) %>%
+  mutate(NA_L3NAME = data.frame(ecoregion_shp)$NA_L3NAME,
+         Shape_Area = data.frame(ecoregion_shp)$Shape_Area) %>%
+  gather(variable, value, -NA_L3NAME, -Shape_Area, -ID) %>%
+  filter(!is.na(value)) %>%
+  separate(variable,
+           into = c("interval", "variable", "timestep"),
+           sep = "_") %>%
+  separate(timestep, into = c("year", "month"), sep = "\\.") %>%
+  group_by(NA_L3NAME, variable, year, month) %>%
+  summarize(wmean = weighted.mean(value, Shape_Area)) %>%
+  ungroup %>%
+  mutate(year = parse_number(year),
+         month = parse_number(month)) %>%
+  arrange(year, month, variable, NA_L3NAME)
+
+write_csv(ecoregion_summaries, "data/processed/ecoregion_summaries.csv")
