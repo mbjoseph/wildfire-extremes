b683f5392b8bddc207200a8e6b7bd4e01edf8b2b
Max Joseph <maxwellbjoseph@gmail.com>
2017-10-05 23:18:11 +0000

    smarter data loading

--- R/03-fit_model.R
+++ R/03-fit_model.R
@@ -22,10 +22,11 @@ stan_d <- list(
   slab_df = 3,
   slab_scale = 5)
 
+pars <- c('beta', 'tau', 'alpha', 'sigma', 'c', 'mu', 'Rho_beta', 'Rho_eps')
 
 
 # Fancy model, nonstationary variance -------------------------------------
-pars <- c('beta', 'tau', 'alpha', 'sigma', 'c', 'mu', 'Rho_beta', 'Rho_eps')
+
 m_init <- stan_model('stan/st-basis-ns-lognorm.stan')
 m_fit <- sampling(m_init,
                    data = stan_d,
@@ -37,30 +38,12 @@ m_fit <- sampling(m_init,
 write_rds(m_fit, 'm_fit.rds')
 
 # Fancy Weibull model --------------------------------------------------------
-w_d <- list(
-  N = N,
-  T = T,
-  L = 2,
-  p = ncol(X),
-  log_area = log(burn_covs$area * 1e-10),
-
-  n_count = nrow(train_counts),
-  counts = train_counts$n_fire,
-  count_idx = count_idx,
-
-  n_fire = nrow(train_burns),
-  sizes = (train_burns$R_ACRES - 1e3) / 1e5,
-  burn_idx = burn_idx,
-
-  n_w = length(sparse_X$w),
-  w = sparse_X$w,
-  v = sparse_X$v,
-  u = sparse_X$u)
-
+weibull_scale_adj <- 1e4
+stan_d$sizes <- train_burns$R_ACRES / weibull_scale_adj
 
 w_init <- stan_model('stan/st-basis-ns-weibull.stan')
 w_fit <- sampling(w_init,
-                  data = w_d,
+                  data = stan_d,
                   pars = pars,
                   cores = 4,
                   init_r = 0.01,
