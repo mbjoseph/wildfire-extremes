2f412f8f2189fba934592c2b5b1a4469e44b8ce8
Max Joseph <maxwellbjoseph@gmail.com>
2017-10-05 23:18:01 +0000

    using a ecoregion shapefile helper function to combat copypasta

---
+++ R/helpers.R
@@ -0,0 +1,16 @@
+
+# Helper functions for fire extremes project ------------------------------
+
+load_ecoregions <- function() {
+  if (!dir.exists("data/raw/us_eco_l3/")) {
+    download.file("ftp://newftp.epa.gov/EPADataCommons/ORD/Ecoregions/us/us_eco_l3.zip",
+                  destfile = "data/raw/us_eco_l3.zip")
+    unzip("data/raw/us_eco_l3.zip",
+          exdir = "data/raw/us_eco_l3/")
+  }
+
+  ecoregion_shp <- readOGR(dsn = "data/raw/us_eco_l3/",
+                           layer = "us_eco_l3")
+
+  ecoregion_shp
+}
