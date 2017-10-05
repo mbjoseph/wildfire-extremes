19d2930312f39d2db34de48c15e7518cb4bc3cc5
Max Joseph <maxwellbjoseph@gmail.com>
2017-10-05 10:27:59 -0600

    include PDSI as a climate variable

--- R/get-ecoregion-summaries.R
+++ R/get-ecoregion-summaries.R
@@ -27,20 +27,27 @@ extract_one <- function(filename, ecoregion_shp) {
     res <- raster::extract(raster::stack(filename), ecoregion_shp,
                     na.rm = TRUE, fun = mean, df = TRUE)
     write.csv(res, file = out_name)
+  } else {
+    res <- read.csv(out_name)
   }
+  res
 }
 
 sfInit(parallel = TRUE, cpus = parallel::detectCores())
 sfExport(list = c("ecoregion_shp"))
 
-extractions <- sfLapply(as.list(tifs[1:4]),
+extractions <- sfLapply(as.list(tifs),
                         fun = extract_one,
                         ecoregion_shp = ecoregion_shp)
 sfStop()
 
+# ensure that they all have the same length
+stopifnot(all(lapply(extractions, nrow) == nrow(ecoregion_shp)))
+
 # convert to a data frame
 library(tidyverse)
-ecoregion_summaries <- extractions %>%
+
+extraction_df <- extractions %>%
   bind_cols %>%
   as_tibble %>%
   mutate(index = ID) %>%
@@ -48,12 +55,27 @@ ecoregion_summaries <- extractions %>%
   rename(ID = index) %>%
   mutate(NA_L3NAME = data.frame(ecoregion_shp)$NA_L3NAME,
          Shape_Area = data.frame(ecoregion_shp)$Shape_Area) %>%
+  dplyr::select(-starts_with('X')) %>%
   gather(variable, value, -NA_L3NAME, -Shape_Area, -ID) %>%
-  filter(!is.na(value)) %>%
+  filter(!is.na(value))
+
+# pdsi has a different naming scheme, need to process differently
+pdsi_df <- extraction_df %>%
+  filter(grepl('pdsi', variable)) %>%
+  separate(variable,
+           into = c('variable', 'year', 'month'),
+           sep = "_") %>%
+  filter(year > 1982)
+
+non_pdsi_df <- extraction_df %>%
+  filter(!grepl('pdsi', variable)) %>%
   separate(variable,
            into = c("interval", "variable", "timestep"),
            sep = "_") %>%
   separate(timestep, into = c("year", "month"), sep = "\\.") %>%
+  select(-interval)
+
+ecoregion_summaries <- full_join(pdsi_df, non_pdsi_df) %>%
   group_by(NA_L3NAME, variable, year, month) %>%
   summarize(wmean = weighted.mean(value, Shape_Area)) %>%
   ungroup %>%
@@ -62,3 +84,4 @@ ecoregion_summaries <- extractions %>%
   arrange(year, month, variable, NA_L3NAME)
 
 write_csv(ecoregion_summaries, "data/processed/ecoregion_summaries.csv")
+
