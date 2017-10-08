7ba6aa18ad1e7259eb81885ab279c4d234865a6c
Max Joseph <maxwellbjoseph@gmail.com>
2017-10-07 21:43:39 -0600

    replace pet with rmin (min. rel. humidity)

--- bash/generate-ecoregion-summaries.sh
+++ bash/generate-ecoregion-summaries.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # pull monthly summaries from S3
-aws s3 cp s3://earthlab-gridmet/pet ../data/processed/pet --recursive
+aws s3 cp s3://earthlab-gridmet/rmin ../data/processed/rmin --recursive
 aws s3 cp s3://earthlab-gridmet/pr ../data/processed/pr --recursive
 aws s3 cp s3://earthlab-gridmet/tmmx ../data/processed/tmmx --recursive
 aws s3 cp s3://earthlab-gridmet/vs ../data/processed/vs --recursive
-aws s3 cp s3://earthlab-gridmet/pdsi/monthly_mean data/processed/pdsi/ --recursive
 
 
 # summarise at the ecoregion level (in parallel, requires 40 cores)
--- bash/generate-monthly-summaries.sh
+++ bash/generate-monthly-summaries.sh
@@ -7,8 +7,8 @@ source metdata_wget.sh
 Rscript --vanilla R/aggregate-climate-data.R
 
 # push monthly GeoTIFFS to AWS S3
-aws s3 cp ../data/processed/pet s3://earthlab-gridmet/pet --recursive
-aws s3 cp ../data/processed/pr s3://earthlab-gridmet/pr --recursive
+aws s3 cp ../data/processed/rmin s3://earthlab-gridmet/rmin --recursive
 aws s3 cp ../data/processed/tmmx s3://earthlab-gridmet/tmmx --recursive
 aws s3 cp ../data/processed/vs s3://earthlab-gridmet/vs --recursive
+aws s3 cp ../data/processed/pr s3://earthlab-gridmet/pr --recursive
 
