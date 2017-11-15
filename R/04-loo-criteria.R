c07322b11d2b74744398800e6b7dc1c480d14f15
Max Joseph <maxwellbjoseph@gmail.com>
2017-11-15 01:38:16 +0000

    compute separate holdout performance metrics for counts and burn areas

--- R/04-loo-criteria.R
+++ R/04-loo-criteria.R
@@ -3,13 +3,32 @@
 library(loo)
 library(tidyverse)
 library(cowplot)
+library(extraDistr)
+
+source('R/02-explore.R')
+source('R/make-stan-d.R')
 
 # possibly fetch from Amazon
-system("aws s3 cp s3://earthlab-mjoseph .. --recursive --exclude '*' --include '*.rds'")
+#system("aws s3 cp s3://earthlab-mjoseph .. --recursive --exclude '*' --include '*.rds'")
 
 model_fits <- list.files(pattern = '*.fit.*\\.rds')
 
 loo_df <- list()
+
+holdout_counts <- count_df %>%
+  mutate(er_ym = paste(NA_L3NAME, ym, sep = "_")) %>%
+  filter(year >= cutoff_year)
+
+count_indices <- match(holdout_counts$er_ym, st_covs$er_ym)
+
+holdout_burns <-  mtbs %>%
+  filter(FIRE_YEAR >= cutoff_year) %>%
+  left_join(st_covs)
+
+burn_indices <- match(holdout_burns$er_ym, holdout_counts$er_ym)
+
+loglik_c <- list()
+loglik_b <- list()
 
 for (i in seq_along(model_fits)) {
   fit_obj <- readRDS(model_fits[i])
@@ -38,13 +57,93 @@ for (i in seq_along(model_fits)) {
   loo_df[[i]] <- loo_total[elements_to_save] %>%
     as_tibble %>%
     mutate(model = model_type) %>%
-    select(model, looic, se_looic)
+    dplyr::select(model, looic, se_looic) %>%
+    mutate(i = i)
+
+  # compute out of sample log likelihood -------------------------------------------------
+  mu_full <- rstan::extract(fit_obj, pars = 'mu_full')[[1]]
+  mu_holdout <- mu_full[, , count_indices]
+  ll_c <- rep(NA, dim(mu_holdout)[1])
+  ll_b <- rep(NA, dim(mu_holdout)[1])
+
+  for (j in seq_along(ll_c)) {
+    if (grepl('ZINB', model_type)) {
+      # zinb count
+      ll_c[j] <- ifelse(holdout_counts$n_fire == 0,
+                        log(plogis(c(mu_holdout[j, 5, ])) +
+                                  (1 - plogis(c(mu_holdout[j, 5, ]))) *
+                          dnbinom(holdout_counts$n_fire,
+                                  size = exp(c(mu_holdout[j, 3, ])),
+                                  mu = exp(c(mu_holdout[j, 4, ])))),
+                        # if not 0, then must be from ninom
+                        log((1 - plogis(c(mu_holdout[j, 5, ])))) +
+                          dnbinom(holdout_counts$n_fire,
+                                size = exp(c(mu_holdout[j, 3, ])),
+                                mu = exp(c(mu_holdout[j, 4, ])),
+                                log = TRUE)) %>%
+        mean()
+    } else {
+      # nb count
+      ll_c[j] <- dnbinom(holdout_counts$n_fire,
+                                     size = exp(c(mu_holdout[j, 3, ])),
+                                     mu = exp(c(mu_holdout[j, 4, ])),
+                         log = TRUE) %>%
+        mean()
+    }
+    # compute log likelihood for heldout burn areas
+    holdout_y <- (holdout_burns$R_ACRES - 1e3) / weibull_scale_adj
+    if (grepl('Gamma', model_type)) {
+      ll_b[j] <- dgamma(holdout_y,
+                        shape = exp(c(mu_holdout[j, 1, burn_indices])),
+                        rate = exp(c(mu_holdout[j, 2, burn_indices])),
+                        log = TRUE) %>%
+        mean()
+    } else if (grepl('Weibull', model_type)) {
+      ll_b[j] <- dweibull(holdout_y,
+                        shape = exp(c(mu_holdout[j, 1, burn_indices])),
+                        scale = exp(c(mu_holdout[j, 2, burn_indices])),
+                        log = TRUE) %>%
+        mean()
+    } else if (grepl('Log-Normal', model_type)) {
+      ll_b[j] <- dlnorm(holdout_y,
+                        meanlog = exp(c(mu_holdout[j, 1, burn_indices])),
+                        sdlog = exp(c(mu_holdout[j, 2, burn_indices])),
+                        log = TRUE) %>%
+        mean()
+    } else if (grepl('Generalized Pareto', model_type)) {
+      ll_b[j] <- dlomax(holdout_y,
+                        lambda = 1 / exp(c(mu_holdout[j, 1, burn_indices])),
+                        kappa = exp(c(mu_holdout[j, 2, burn_indices])),
+                        log = TRUE) %>%
+        mean()
+    }
+  }
+
+  loglik_c[[i]] <- ll_c
+  loglik_b[[i]] <- ll_b
 
   # clean up
-  rm(fit_obj, loglik_burn_area, loo_total, loglik_counts)
+  rm(fit_obj, loglik_burn_area, loo_total, loglik_counts, mu_full, mu_holdout)
   gc()
 }
 
 bind_rows(loo_df) %>%
   arrange(looic) %>%
   write_csv('loo-ic-table.csv')
+
+model_names <- lapply(loo_df, function(x) paste(x$model, x$i)) %>%
+  unlist()
+
+names(loglik_c) <- model_names
+plot(density(loglik_c[[1]]), xlim = c(-.56, -.4), ylim = c(0, 60))
+for (i in 2:length(loglik_c)) {
+  lines(density(loglik_c[[i]]), col = i)
+}
+legend('topleft', legend = model_names, lty = 1, col = seq_along(loglik_c), bty = 'n')
+
+names(loglik_b) <- model_names
+plot(density(log(-loglik_b[[1]])), xlim = c(-1, 0), ylim = c(0, 30))
+for (i in 2:length(loglik_b)) {
+  lines(density(log(-loglik_b[[i]]), na.rm = TRUE), col = i)
+}
+legend('topright', legend = model_names, lty = 1, col = seq_along(loglik_c), bty = 'n')
