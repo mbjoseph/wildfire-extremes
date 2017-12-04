fce70f4ba2d96f4e9f27e83c0314d882c80d7398
Max Joseph <maxwellbjoseph@gmail.com>
2017-12-04 22:54:04 +0000

    saving density ppc as one multipanel fig

--- R/04-burn-area-model-comps.R
+++ R/04-burn-area-model-comps.R
@@ -26,15 +26,22 @@ for (i in seq_along(burn_area_fits)) {
   holdout_ba_loglik[[i]] <- post$holdout_loglik_b %>%
     reshape2::melt(varnames = c('iter', 'idx')) %>%
     as_tibble %>%
+    group_by(iter) %>%
+    summarize(value = sum(value)) %>%
     mutate(model = burn_area_fits[i])
+
   train_ba_loglik[[i]] <- post$loglik_f %>%
     reshape2::melt(varnames = c('iter', 'idx')) %>%
     as_tibble %>%
+    group_by(iter) %>%
+    summarize(value = sum(value)) %>%
     mutate(model = burn_area_fits[i])
+
   holdout_ba_rep[[i]] <- post$holdout_rep %>%
     reshape2::melt(varnames = c('iter', 'idx')) %>%
     as_tibble %>%
     mutate(model = burn_area_fits[i])
+
   train_ba_rep[[i]] <- post$size_rep %>%
     reshape2::melt(varnames = c('iter', 'idx')) %>%
     as_tibble %>%
@@ -47,25 +54,16 @@ holdout_ba_loglik <- bind_rows(holdout_ba_loglik) %>%
 train_ba_loglik <- bind_rows(train_ba_loglik) %>%
   mutate(train = TRUE)
 
-ba_ll_df <- holdout_ba_loglik %>%
-  group_by(iter, model, train) %>%
-  summarize(mean_ll = mean(value))
-
-train_ba_ll_df <- train_ba_loglik %>%
-  group_by(iter, model, train) %>%
-  summarize(mean_ll = mean(value))
-
 # data frame for plotting colors
 cols <- c('Gamma' = 'purple',
-          'Weibull' = 'darkgreen',
+          'Weibull' = 'orange',
           'Generalized Pareto' = 'red',
-          'Tapered Pareto' = 'orange',
+          'Tapered Pareto' = 'limegreen',
           'Lognormal' = 'dodgerblue')
 
 
-ba_ll_df %>%
-  full_join(train_ba_ll_df) %>%
-  ungroup %>%
+holdout_ba_loglik %>%
+  full_join(train_ba_loglik) %>%
   mutate(train = ifelse(train == TRUE, 'train', 'test'),
          Distribution = case_when(
            grepl('gamma', .$model) ~ 'Gamma',
@@ -74,7 +72,7 @@ ba_ll_df %>%
            grepl('_tpareto', .$model) ~ 'Tapered Pareto',
            grepl('weibull', .$model) ~ 'Weibull'
          )) %>%
-  spread(train, mean_ll) %>%
+  spread(train, value) %>%
   ggplot(aes(x = train, y = test, color = Distribution)) +
   geom_point(alpha = .05) +
   xlab('Log likelihood: training set') +
@@ -82,16 +80,17 @@ ba_ll_df %>%
   scale_color_manual('Burn area distribution', values = cols) +
   guides(colour = guide_legend(override.aes = list(alpha = 1))) +
   theme(legend.justification = c(1, 0), legend.position = c(1, 0)) +
-  ylim(c(-11.4, -9.6)) +
+  ylim(c(-26000, -22200)) +
   theme_minimal()
+ggsave(filename = 'fig/loglik.png', width = 6, height = 4)
 
 ## Generate plots to evaluate distributional assumptions
 train_ba_rep <- train_ba_rep %>%
   bind_rows
 
 # Whole distribution
-train_ba_rep %>%
-  filter(iter < 1001) %>%
+den_plot <- train_ba_rep %>%
+  filter(iter < 101) %>%
   mutate(Distribution = case_when(
     grepl('gamma', .$model) ~ 'Gamma',
     grepl('lognormal', .$model) ~ 'Lognormal',
@@ -106,7 +105,7 @@ train_ba_rep %>%
   stat_density(aes(color = Distribution), geom="line", alpha = .1, position = 'identity') +
   scale_x_log10() +
   scale_color_manual('Burn area distribution', values = cols) +
-  facet_wrap(~Distribution) +
+  facet_wrap(~Distribution, nrow = 1) +
   stat_density(geom = 'line', position = 'identity',
                inherit.aes = FALSE,
                aes(x = actual_sizes),
@@ -124,10 +123,10 @@ train_ba_rep %>%
 obs_ccdf <- tibble(values = sort(stan_d$sizes)) %>%
   mutate(ccdf = seq(n(), 1, -1) / n()) %>%
   filter(ccdf <= .01)
+gc()
 
-train_ba_rep %>%
-  filter(!grepl('gamma', model),
-         !grepl('weibull', model)) %>%
+tail_plot <- train_ba_rep %>%
+  filter(iter < 1001) %>%
   group_by(iter, model) %>%
   arrange(iter, model, value) %>%
   mutate(ccdf = seq(n(), 1, -1) / n()) %>%
@@ -147,16 +146,24 @@ train_ba_rep %>%
     grepl('weibull', .$model) ~ 'Weibull'
   )) %>%
   ggplot(aes(med, ccdf, color = Distribution)) +
+  theme_minimal() +
   scale_color_manual('Burn area distribution', values = cols) +
   geom_point() +
   geom_errorbarh(aes(xmin = lo, xmax = hi)) +
   geom_errorbarh(aes(xmin = q25, xmax = q75), size = 1.25) +
   scale_x_log10() +
   scale_y_log10() +
-  facet_wrap(~Distribution) +
+  facet_wrap(~Distribution, nrow = 1) +
   coord_cartesian(xlim = c(1e5, 1e8),
                   ylim = c(1e-4, .01)) +
   theme(legend.position = 'none') +
-  geom_point(data = obs_ccdf, color = 'black', aes(x = values), shape = 1) +
+  geom_point(data = obs_ccdf, color = 'black', aes(x = values), shape = 1, size = .5) +
   xlab('Burn area exceedance (acres)') +
-  ylab('Complementary cumulative distribution function')
+  ylab('Complementary CDF') +
+  theme(
+    strip.background = element_blank(),
+    strip.text.x = element_blank()
+  )
+
+plot_grid(den_plot, tail_plot, nrow = 2)
+ggsave(filename = 'fig/ppc-density-funs.png', width = 12, height = 5)
